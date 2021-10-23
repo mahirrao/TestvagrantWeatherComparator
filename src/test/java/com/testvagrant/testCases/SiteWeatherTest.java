debfad7c2589a4e32a5499de60a00117140ebbbc
mahirrao <md.ahirrao@gmail.com>
2021-10-23 19:33:24 +0530

    First successfully running test

--- src/test/java/com/testvagrant/testCases/SiteWeatherTest.java
+++ src/test/java/com/testvagrant/testCases/SiteWeatherTest.java
@@ -4,15 +4,19 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.testvagrant.base.Page;
+import com.testvagrant.pages.actions.HomePage;
 
 public class SiteWeatherTest extends BaseTest
 {
 	@Test
 	public void siteWeatherTest()
 	{
-		
+		String location = "Pune Maharashtra IN";
+		HomePage home = new HomePage();
+		String currentTemp = home.searchWeather(location).getCurrentTemperature();
+		System.out.println("Current Temperature at " +location+ " is: " +currentTemp );
+		Page.log.info("Current Temperature at " +location+ " is: " +currentTemp );
 	}
-	
 	
 }
 
